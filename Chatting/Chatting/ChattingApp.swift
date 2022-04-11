c9502f0ca87a50dc3c70a7ad3741c62d7cc675f2
KangKyung <rudgns0508@naver.com>
2022-04-11 21:35:39 +0900

    Project initial setting
    - 프로젝트 생성
    - 패키징을위한 기본폴더 생성
    - swiftlint 추가
    - .gitignore 추가
    - TCA 라이브러리 추가

---
+++ Chatting/Chatting/ChattingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingApp.swift
+//  Chatting
+//
+//  Created by 강경 on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct ChattingApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
